9551f0358d6a50538342150e5fc2a420b5fc97cc
trungnguyen1997 <61753074+trungnguyen1997@users.noreply.github.com>
2020-07-06 18:48:07 +0700

    dc

---
+++ model/downvideoyoutube.js
@@ -0,0 +1,84 @@
+const fs = require('fs')
+const youtubedl = require('youtube-dl');
+const videosadd = require('./videos');
+const { resolve } = require('path');
+const { isNull } = require('util');
+module.exports = {
+    downvideosbyID: async id => {
+        var entity = {};
+        entity.idvideo = id;
+        var idInDb;
+        const video = await youtubedl('https://www.youtube.com/watch?v=' + id,
+            ['--format=18'],
+            { cwd: __dirname })
+        try {
+            video.on('info', function async(info) {
+                var checkdownload = true;
+                if (info.size > 50925141) { checkdownload = false }
+                var datetemp = getdatenow();
+                //console.log(datetemp);
+                if (Number(datetemp) === Number(info.upload_date)) {
+                    checkdownload = true;
+                }
+                if (checkdownload) {                   
+                    console.log('Download started:');
+                    console.log('filename: ' + info._filename.replace("-" + id, ""));
+                    console.log("size: ",info.size);
+                    //console.log(info.upload_date);
+                    console.log("------------------------------------------------");
+                    // console.log(Number(info.upload_date));
+                    entity.videosname = info._filename.replace("-" + id, "");
+                    var today = new Date();
+                    var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+                    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+                    var dateTime = date + ' ' + time;
+                    entity.datevideo = dateTime;
+                    entity.dateexport = Number(info.upload_date);
+                    entity.like = 0;
+                    entity.views = 0;
+                    entity.idchannel = info.channel_id;
+                    let promise = new Promise(async (resolve, reject) => {
+                        //console.log(entity);   
+                        idInDb = await videosadd.addvideo(entity);
+                        resolve(idInDb);
+                    })
+                    return promise.then(async (idInDb) => {
+                        await video.pipe(fs.createWriteStream('./video/' + idInDb + '.mp4'));
+                        console.log('tải xong!');
+                        sleep(500);
+                        return true;
+                    });
+                } else {
+                    //sleep(3000);
+                    console.log('video quá nặng');
+                    return false;
+                }
+            }
+            )
+        } catch (error) {
+            return false;
+        }
+
+    }
+
+}
+function sleep(time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+
+}
+function getdatenow() {
+    var today = new Date();
+    var datetemp;
+    if (Number(today.getMonth()) >= 10 && Number(today.getDate()) >= 10)
+        datetemp = today.getFullYear() + (today.getMonth() + 1) + today.getDate();
+    if (Number(today.getMonth()) < 10 && Number(today.getDate()) > 10)
+        datetemp = today.getFullYear() + '0' + (today.getMonth() + 1) + today.getDate();
+    if (Number(today.getMonth()) >= 10 && Number(today.getDate()) < 10)
+        datetemp = today.getFullYear() + (today.getMonth() + 1) + '0' + today.getDate();
+    if (Number(today.getMonth()) < 10 && Number(today.getDate()) < 10)
+        datetemp = today.getFullYear() + '0' + (today.getMonth() + 1) + '0' + today.getDate();
+    return datetemp;
+}
+
+
+
