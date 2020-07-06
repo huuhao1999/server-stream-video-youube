e7b6fb8048502131352872e628cf711178d3556c
trungnguyen1997 <61753074+trungnguyen1997@users.noreply.github.com>
2020-07-06 11:36:00 +0700

    f

--- Run/download-video_playlist.js
+++ Run/download-video_playlist.js
@@ -26,7 +26,7 @@ const DownLoadVideos = {
 
                     console.log(i);
                     await downvideo.downvideosbyID(xxxa[i]);
-                    await sleep(4000);
+                    await sleep(1000);
                 }
                 if (checlist[i] === true) console.log("video exists nha");
             }
