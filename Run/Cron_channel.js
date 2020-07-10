b9950b40761531b051ab232c16c41a9109e1fdd3
trungnguyen1997 <61753074+trungnguyen1997@users.noreply.github.com>
2020-07-10 15:16:18 +0700

    cd

--- Run/Cron_channel.js
+++ Run/Cron_channel.js
@@ -1,7 +1,7 @@
 const { spawn } = require('child_process');
 const schedule = require('node-schedule');
 
-schedule.scheduleJob('*/50 * * * *', function() {
+schedule.scheduleJob('0 */1 * * *', function() {
     console.log("running....")
   spawn('node', ['Run_channel.js']),function(err, stdout, stderr) { 
     console.log(stdout); 
